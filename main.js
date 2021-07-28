e01a8e7b4254436b7cdea9c585a3b1c4fe2c11af
Shalini-jha-dev <74014550+Shalini-jha-dev@users.noreply.github.com>
2021-07-28 14:28:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.position(650, 400);
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save("MyFilterImage.png");
+}
